6ac7680cb4e0f25692b55200e3fe389dddacae9b
Sourav sarkar <153831922+Sourav-6763@users.noreply.github.com>
2025-09-01 12:37:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int kthElement(int a[], int b[], int k) {
+        ArrayList<Integer> res=new ArrayList<>();
+        for(int i=0;i<a.length;i++){
+            res.add(a[i]);
+        }
+        for(int i=0;i<b.length;i++){
+            res.add(b[i]);
+        }
+        Collections.sort(res);
+        return res.get(k-1);
+    }
+}
