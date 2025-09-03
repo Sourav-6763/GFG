df2cc26ab08624ed9d7e4c3f0971d608737c0ce5
Sourav sarkar <153831922+Sourav-6763@users.noreply.github.com>
2025-09-03 11:35:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Peak element/peak-element.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int peakElement(int[] arr) {
+         int n = arr.length;
+       if (n == 1) return 0;
+       if(arr[0]>=arr[1])return 0;
+       
+        for(int i=1;i<arr.length-1;i++){
+            if(arr[i-1]<arr[i] && arr[i]>arr[i+1]){
+                return i;
+            }
+        }
+        return arr.length-1;
+    }
+}
