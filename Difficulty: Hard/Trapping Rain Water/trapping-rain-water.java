b5233948446eee4471f7e92f1fd26e00e4acb521
Sourav sarkar <153831922+Sourav-6763@users.noreply.github.com>
2025-11-02 19:03:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Trapping Rain Water/trapping-rain-water.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxWater(int arr[]) {
+        int sum=0;
+        int n=arr.length;
+        int[] leftMax =new int [n];
+        int[] rightMax =new int [n];
+        int lmax=arr[0];
+        for(int i=0;i<n;i++){
+            lmax=Math.max(lmax,arr[i]);
+            leftMax[i]=lmax;
+        }
+        int rmax=arr[n-1];
+        for(int i=n-1;i>=0;i--){
+            rmax=Math.max(rmax,arr[i]);
+            rightMax[i]=rmax;
+        }
+        for(int i=0;i<n;i++){
+            int min=Math.min(leftMax[i],rightMax[i]);
+            sum+=min-arr[i];
+        }
+        return sum;
+    }
+}
